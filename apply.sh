f6f1c5ef3739a1781961793ce654f0364296a072
Sebastian Krueger <sebastian.krueger@apitalent.co.nz>
2015-04-13 02:54:38 +1200

    useful apply script that will create cloud formation stacks for the labs.

---
+++ apply.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+function getOutputsFromNetworkStack {
+	eval $(aws cloudformation describe-stacks --stack-name proarchtraining-lab1-network --output text | grep OUTPUTS | \
+			awk 'BEGIN{FS="[ \t,]"}; \
+				{if ($4=="VpcId") \
+					{print "export VpcId="$5} \
+				else if ($5=="PublicSubnetIds") \
+					{print "export PubASubnetId="$6"\nexport PubBSubnetId="$7} \
+				else if ($6=="PrivateRouteTableId") \
+					{print "export PrivateRouteTableId="$7} \
+				}')
+}
+
+KeyName=proarchtraining_sebastiankrueger
+
+case $1 in 
+	create-network)
+		aws cloudformation create-stack \
+			--stack-name proarchtraining-lab1-network \
+			--template-body "file://${DIR}/basicvpc-seb.template.json"
+	;;
+	update-network)
+		aws cloudformation update-stack \
+			--stack-name proarchtraining-lab1-network \
+			--template-body "file://${DIR}/basicvpc-seb.template.json"
+	;;
+	create-bastion-nat)
+
+		getOutputsFromNetworkStack
+
+		aws cloudformation create-stack \
+			--stack-name proarchtraining-lab1-bastion-nat \
+			--template-body "file://${DIR}/bastion-nat-seb.template.json" \
+			--parameters \
+				ParameterKey=KeyName,ParameterValue=${KeyName} \
+				ParameterKey=VpcId,ParameterValue=${VpcId} \
+				ParameterKey=PubASubnetId,ParameterValue=${PubASubnetId} \
+				ParameterKey=PubBSubnetId,ParameterValue=${PubBSubnetId} \
+				ParameterKey=PrivateRouteTableId,ParameterValue=${PrivateRouteTableId}
+	;;
+	update-bastion-nat)
+		
+		getOutputsFromNetworkStack
+
+		aws cloudformation update-stack \
+			--stack-name proarchtraining-lab1-bastion-nat \
+			--template-body "file://${DIR}/bastion-nat-seb.template.json" \
+			--parameters \
+				ParameterKey=KeyName,ParameterValue=${KeyName} \
+				ParameterKey=VpcId,ParameterValue=${VpcId} \
+				ParameterKey=PubASubnetId,ParameterValue=${PubASubnetId} \
+				ParameterKey=PubBSubnetId,ParameterValue=${PubBSubnetId} \
+				ParameterKey=PrivateRouteTableId,ParameterValue=${PrivateRouteTableId}
+	;;
+	*)
+        echo $"Usage: $0 {[create|update]-[network|bastion-nat]}"
+        exit 1
+esac
